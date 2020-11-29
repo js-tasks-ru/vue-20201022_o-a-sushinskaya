a9e63704cbb29e91325b9c299ab5d4fbc2302989
Oksana Sushinskaya <sushinskayaoksana@gmail.com>
2020-11-30 00:14:00 +0200

    Add task 02-MeetupPageComponent

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,24 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+      <MeetupAgendaItem
+        v-for="agendaItem in agenda"
+        :key="agendaItem.id"
+        :agendaItem="agendaItem" />
     </div>`,
 
-  // components
+  components: {
+    MeetupAgendaItem
+  },
 
-  // props
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    }
+  },
 };
